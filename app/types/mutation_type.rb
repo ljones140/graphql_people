e1422bd3c4f6767093389818e21206e7cebba958
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2018-04-29 22:07:02 +0100

    Use input types in mutation

--- app/types/mutation_type.rb
+++ app/types/mutation_type.rb
@@ -3,11 +3,30 @@ MutationType = GraphQL::ObjectType.define do
 
   field :addPerson, PersonType do
     description "Adds a person"
-    argument :name, !types.String
+    argument :person, PersonInputType
 
-    resolve ->(obj, args, ctx) {
-      person = Person.create(name: args[:name])
+    resolve ->(obj, args, ctx) do
+      person = Person.create(name: args[:person][:name], surname: args[:person][:surname])
+      pet = args[:person][:pet_name]
+      person.pets.create(name: pet) if pet.present?
       person
-    }
+    end
+  end
+end
+
+PersonInputType = GraphQL::InputObjectType.define do
+  name "PersonInputType"
+  description "Properties for creating a Person"
+
+  argument :name, !types.String do
+    description "Name of person"
+  end
+
+  argument :surname, !types.String do
+    description "Surname of person"
+  end
+
+  argument :pet_name, types.String do
+    description "Name of pet"
   end
 end
