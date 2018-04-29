6ebbbb3068107cf660c7dc47e03936f136a491f6
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2018-04-29 21:27:39 +0100

    Add graphiql browser IDE

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   post '/query' => 'application#query'
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/query'
+  end
 end
